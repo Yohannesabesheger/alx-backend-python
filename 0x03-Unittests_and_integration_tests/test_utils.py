ea3745278c53aa4a129afa860b9b706bf9c56ae6
beshegercom@gmail.com <beshegercom@gmail.com>
2025-05-28 20:16:20 +0300

    parameter

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -9,9 +9,8 @@ class TestAccessNestedMap(unittest.TestCase):
     """Unit test class for access_nested_map"""
 
     @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
-    ])
+    ({"a": 1}, ("a",), 1),  # Corrected expected result
+])
+
     def test_access_nested_map(self, nested_map, path, expected):
         self.assertEqual(access_nested_map(nested_map, path), expected)
