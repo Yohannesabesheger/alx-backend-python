0e5750213da7d4e9fb87c5424132a8ecd9eecbd8
beshegercom@gmail.com <beshegercom@gmail.com>
2025-05-30 23:13:59 +0300

    dddd

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -18,14 +18,7 @@ from fixtures import (
     org_payload, repos_payload, expected_repos, apache2_repos
 )
 
-@parameterized_class([
-    {
-        'org_payload': org_payload,
-        'repos_payload': repos_payload,
-        'expected_repos': expected_repos,
-        'apache2_repos': apache2_repos
-    }
-])
+
 
 class TestGithubOrgClient(unittest.TestCase):
     """
