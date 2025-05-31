9ce83d34808b42031552f346ee33633ac253c560
yohannesm <jonmersha@gmail.com>
2025-05-31 10:24:23 +0300

    Api Root Implementations

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
 
-# Create your views here.
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for listing and creating conversations.
+    """
+    serializer_class = ConversationSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # List conversations the current user is part of
+        return Conversation.objects.filter(participants=self.request.user).distinct()
+
+    def perform_create(self, serializer):
+        # Automatically include the requesting user in participants
+        conversation = serializer.save()
+        if self.request.user not in conversation.participants.all():
+            conversation.participants.add(self.request.user)
+        conversation.save()
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for listing and creating messages in conversations.
+    """
+    serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # Only messages from conversations the user is part of
+        return Message.objects.filter(conversation__participants=self.request.user)
+
+    def perform_create(self, serializer):
+        conversation = serializer.validated_data.get("conversation")
+        if self.request.user not in conversation.participants.all():
+            return Response(
+                {"detail": "You are not a participant in this conversation."},
+                status=status.HTTP_403_FORBIDDEN
+            )
+        serializer.save(sender=self.request.user)
