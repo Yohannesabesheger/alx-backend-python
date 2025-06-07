b6e6bc32792cd5f40242f1a34f4f68d7f6eebf47
yohannesm <jonmersha@gmail.com>
2025-06-07 17:26:00 +0300

    Api Ducemntations

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,16 +1,15 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from rest_framework_nested.routers import NestedDefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
 # Root router for conversations
-router = NestedDefaultRouter()
+router = DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 
-# Nested router: messages inside conversations
-convo_router = NestedDefaultRouter(
-    router, r'conversations', lookup='conversation')
-convo_router.register(r'messages', MessageViewSet,
-                      basename='conversation-messages')
+# Nested router for messages inside conversations
+convo_router = NestedDefaultRouter(router, r'conversations', lookup='conversation')
+convo_router.register(r'messages', MessageViewSet, basename='conversation-messages')
 
 urlpatterns = [
     path('', include(router.urls)),
--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -20,7 +20,26 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from rest_framework import permissions
 
+from django.http import HttpResponse
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Messaging API",
+        default_version='v1',
+        description="API documentation for the Messaging app",
+        contact=openapi.Contact(email="youremail@example.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+def home(request):
+    return HttpResponse("Welcome to Messaging API")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,4 +47,12 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', home),  # <-- This handles the root URL
+
+    # Swagger UI:
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
+    # ReDoc UI:
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 ]
