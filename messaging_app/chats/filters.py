c1673d61bd75ab778001c88d6e9b68b54fd8f93c
yohannesm <jonmersha@gmail.com>
2025-06-07 16:10:23 +0300

    paginations number

--- messaging_app/chats/filters.py
+++ messaging_app/chats/filters.py
@@ -1,23 +1,17 @@
 import django_filters
 from django_filters.rest_framework import FilterSet, filters
-from .models import Message, Conversation
+from .models import Message
 
 class MessageFilter(FilterSet):
-    # Filter messages by date range
-    start_date = filters.DateTimeFilter(field_name="sent_at", lookup_expr='gte')
-    end_date = filters.DateTimeFilter(field_name="sent_at", lookup_expr='lte')
+    # Filter messages by conversation participants (username or id)
+    conversation_participant = django_filters.CharFilter(
+        field_name='conversation__participants__username', lookup_expr='icontains'
+    )
 
-    # Optionally filter by conversation participant username or email
-    participant = filters.CharFilter(method='filter_by_participant')
+    # Filter messages by datetime range
+    sent_at__gte = django_filters.DateTimeFilter(field_name='sent_at', lookup_expr='gte')
+    sent_at__lte = django_filters.DateTimeFilter(field_name='sent_at', lookup_expr='lte')
 
     class Meta:
         model = Message
-        fields = ['conversation', 'start_date', 'end_date']
-
-    def filter_by_participant(self, queryset, name, value):
-        # Filter messages where sender or receiver username or email matches value
-        return queryset.filter(
-            conversation__participants__username__icontains=value
-        ) | queryset.filter(
-            conversation__participants__email__icontains=value
-        )
+        fields = ['conversation_participant', 'sent_at__gte', 'sent_at__lte']
