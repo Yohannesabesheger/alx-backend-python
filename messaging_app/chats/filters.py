5a014f533bffc2510145e0dd5d161b0fc8d72312
yohannesm <jonmersha@gmail.com>
2025-06-07 16:12:58 +0300

    error

--- messaging_app/chats/filters.py
+++ messaging_app/chats/filters.py
@@ -1,14 +1,11 @@
 import django_filters
-from django_filters.rest_framework import FilterSet, filters
+from django_filters.rest_framework import FilterSet
 from .models import Message
 
 class MessageFilter(FilterSet):
-    # Filter messages by conversation participants (username or id)
     conversation_participant = django_filters.CharFilter(
         field_name='conversation__participants__username', lookup_expr='icontains'
     )
-
-    # Filter messages by datetime range
     sent_at__gte = django_filters.DateTimeFilter(field_name='sent_at', lookup_expr='gte')
     sent_at__lte = django_filters.DateTimeFilter(field_name='sent_at', lookup_expr='lte')
 
