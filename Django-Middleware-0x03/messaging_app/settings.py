492eb49af461682862026803f7f26705d4778b73
beshegercom@gmail.com <beshegercom@gmail.com>
2025-06-08 16:00:26 +0300

    middile ware and requiest logging

--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'chats.middleware.RequestLoggingMiddleware',  # Custom middleware for chat app
 ]
 
 ROOT_URLCONF = 'messaging_app.urls'
