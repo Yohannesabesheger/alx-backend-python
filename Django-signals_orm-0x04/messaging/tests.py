2f3915fb8eda52ce8de9bf0243a2ca282d8b2c81
beshegercom@gmail.com <beshegercom@gmail.com>
2025-06-12 06:25:04 +0300

    messagin app conffigured

--- Django-signals_orm-0x04/messaging/tests.py
+++ Django-signals_orm-0x04/messaging/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Message, Notification
 
-# Create your tests here.
+class MessageSignalTestCase(TestCase):
+    def setUp(self):
+        self.sender = User.objects.create_user(username='sender', password='pass123')
+        self.receiver = User.objects.create_user(username='receiver', password='pass123')
+
+    def test_notification_created_on_message(self):
+        """Ensure a Notification is created when a Message is sent."""
+        msg = Message.objects.create(
+            sender=self.sender,
+            receiver=self.receiver,
+            content="Test message"
+        )
+        notif = Notification.objects.filter(user=self.receiver, message=msg)
+        self.assertEqual(notif.count(), 1)
+        self.assertFalse(notif.first().is_read)
