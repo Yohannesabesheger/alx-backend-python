2b7ef66769ba4abb8f428cfb1096a2c22b9ca35e
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 <jonmersha@gmail.com>
2025-06-12 09:20:58 +0300

    ensiruction 2

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -18,3 +18,11 @@ class Notification(models.Model):
 
     def __str__(self):
         return f"Notification for {self.user.username} on message {self.message.id}"
+
+class MessageHistory(models.Model):
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='history')
+    old_content = models.TextField()
+    edited_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"History for Message ID {self.message.id}"
--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,6 +1,8 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Message, Notification
+from .models import Message, Notification, MessageHistory
+from django.test import TestCase
+from django.contrib.auth.models import User
 
 @receiver(post_save, sender=Message)
 def create_notification(sender, instance, created, **kwargs):
@@ -10,3 +12,18 @@ def create_notification(sender, instance, created, **kwargs):
             user=instance.receiver,
             message=instance
         )
+class MessageEditLoggingTest(TestCase):
+    def setUp(self):
+        self.user1 = User.objects.create_user(username='user1', password='testpass')
+        self.user2 = User.objects.create_user(username='user2', password='testpass')
+        self.message = Message.objects.create(sender=self.user1, receiver=self.user2, content="Original Message")
+
+    def test_edit_logging_creates_history(self):
+        self.message.content = "Edited Message"
+        self.message.save()
+
+        history = MessageHistory.objects.filter(message=self.message)
+        self.assertEqual(history.count(), 1)
+        self.assertEqual(history.first().old_content, "Original Message")
+        self.assertTrue(self.message.edited)
+
