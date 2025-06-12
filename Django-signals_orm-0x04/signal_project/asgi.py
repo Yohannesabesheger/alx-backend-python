b6ef82d6dc88b99e5ca7b82d16729fd4add3c2a2
beshegercom@gmail.com <beshegercom@gmail.com>
2025-06-12 06:17:33 +0300

    app created

--- Django-signals_orm-0x04/signal_project/asgi.py
+++ Django-signals_orm-0x04/signal_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for messaging project.
+ASGI config for signal_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'messaging.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'signal_project.settings')
 
 application = get_asgi_application()
